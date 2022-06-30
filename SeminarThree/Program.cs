089c4ad7aedd84d302211c3369fe8a6fefcb3935
SlavaMonochrom <107564935+SlavaMonochrom@users.noreply.github.com>
2022-06-30 22:36:00 +0300

    Seminar 3

---
+++ SeminarThree/Program.cs
@@ -0,0 +1,26 @@
+﻿void ShowAerea(int quart);
+{
+    if(quart>=1&& quart>=4);
+    {
+        if(quart==1) Console.WriteLine(In this ) 
+        
+
+
+        
+
+
+    }
+
+
+}
+
+Console.WriteLine("Введите число: " + );
+Console.Convert.ToInt32(ReadLine());
+ShowAerea(quart); 
+
+
+//принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и номер четверти плоскости, в которой находится эта точка
+//принимает на вход число (N) и выдаёт квадраты чисел от 1 до N.
+
+
+
