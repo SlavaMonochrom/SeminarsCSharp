86986f1b8ec3df9cdbcc128fc71e0e9f056f4d98
SlavaMonochrom <107564935+SlavaMonochrom@users.noreply.github.com>
2022-07-21 23:38:16 +0300

    9

--- Seminar 9/Program.cs
+++ Seminar 9/Program.cs
@@ -1,7 +1,7 @@
 ﻿void ShowNums(int n) //от одного до n
 {
-    if(i > 1)
-        ShowNums(n-1);
+    if (i > 1)
+        ShowNums(n - 1);
     Console.Write(n + " ");
 
 }
@@ -15,17 +15,46 @@ ShowNums(5);
 
 int sumOfDigits(int n)
 {
-    if(n>=10)
+    if (n >= 10)
     {
-        return n%10 + sumOfDigits(n/10);
+        return n % 10 + sumOfDigits(n / 10);
 
     }
     else return n;
 }
-    
-ShowNums(n-1);
+
+ShowNums(n - 1);
 Console.Write(n + " ");
 Console.WriteLine(sumOfDigits(12345));
 
 
 //Задайте значения M и N. выведет все натуральные в промежутке от M до N.
+void ShouNum(int m, int n)
+{
+
+    if (n > m)
+    {
+        ShouNum(m, n - 1);
+    }
+    Console.Write(n + " ");
+
+}
+ShouNum(1, 10);
+
+
+
+//Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+//a*bn   
+
+//a=2   b=3 2*2*2
+
+int Stepen(int a, int b)
+{
+    if (b > 1)
+    {
+        return a * Stepen(a, b - 1);
+    }
+    else return a;
+}
+Console.WriteLine();
+Console.WriteLine(Stepen(4, 2));
